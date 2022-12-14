33697454385034af1383df4ad30324cfbf923b4d
Vinay Rao <vrao8912@gmail.com>
2022-12-14 13:52:12 +0100

    connectedf with mongoose db

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const brewrecipeRoutes = require("./routes/brewrecipes");
+const mongoose = require("mongoose");
+// Set the strictQuery option to false
+mongoose.set('strictQuery', false);
 
 //setting up basic middleware and ivoking next so the following steps will be carried out
 //we can log the path and request
@@ -14,7 +17,15 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/brewrecipes", brewrecipeRoutes);
 
-//Port
-app.listen(process.env.PORT, () => {
-  console.log(`Server lsitening on port`, process.env.PORT);
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //Port listen to requests
+    app.listen(process.env.PORT, () => {
+      console.log(`Server lsitening on port`, process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });

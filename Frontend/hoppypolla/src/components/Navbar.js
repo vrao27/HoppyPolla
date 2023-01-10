951ca5dff0765e0715766e927f966315993ea03b
Vinay Rao <vrao8912@gmail.com>
2023-01-10 19:52:21 +0100

    comm

--- Frontend/hoppypolla/src/components/Navbar.js
+++ Frontend/hoppypolla/src/components/Navbar.js
@@ -4,13 +4,19 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <header>
-      <div className="container">
+      <div className="nav-container">
+        <h1>HoppyPolla</h1>
         <Link to="/">
-            <h1>HoppyPolla</h1>
+            Home
         </Link>
+        <Link to="/brew">Brew</Link>
+        <Link to="/brewing-101">Brewing 101</Link>
+        <Link to="/create-brew">Create Brew</Link>
+        <Link to="/contact">Contact</Link>
       </div>
     </header>
   );
 };
 
-export default Navbar;
+
+export default Navbar

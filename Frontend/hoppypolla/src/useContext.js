a8337accdd49322551bb3db212d5a56c282e59e8
Vinay Rao <vrao8912@gmail.com>
2023-01-10 11:04:25 +0100

    commit

--- Frontend/hoppypolla/src/useContext.js
+++ Frontend/hoppypolla/src/useContext.js
@@ -1,7 +1,35 @@
-import { createContext } from "react";
+import { useState, createContext, useContext } from "react";
 
 export const BrewRecipeContext = createContext();
 
+export const brewRecipeReducer = (state, action) => {
+ switch (action.type) {
+    case 'SET_CURRENTRECIPE': 
+        return {
+            brewRecipe: action.payload
+        }
+
+    case 'CREATE_BREWRECIPE': 
+        return {
+             brewRecipe: [action.payload, ...state.brewRecipe]
+        }
+        default: 
+        return state
+ }
+}
+
 export const BrewRecipeContextProvider = ({ children }) => {
-  return <BrewRecipeContext.Provider>{children}</BrewRecipeContext.Provider>;
+
+    const [state, dispatch] = useReducer(brewRecipeReducer, {
+    brewRecipes: null
+    })
+
+//dispatch describes the State change
+
+
+  return (
+  <BrewRecipeContext.Provider value = {{state, dispatch}>
+  { children }
+  </BrewRecipeContext.Provider>
+  )
 };

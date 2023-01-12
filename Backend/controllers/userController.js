f7e5377b35e101abc7e55e2fae4b57b01eaf4ac0
Vinay Rao <vrao8912@gmail.com>
2023-01-12 12:45:15 +0100

    comm

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,9 +1,28 @@
 const User = require("../Models/usermodel");
+const jwt = require("jsonwebtoken");
+
+// create token to be used in login and signup controller functions
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //login user
 
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    //create a toke
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //signupuser
@@ -14,7 +33,11 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    res.status(200).json({ email, user });
+    //create a toke
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }

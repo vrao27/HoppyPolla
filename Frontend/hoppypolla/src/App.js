70388aa3164c0bc91dad8f5e812e9faa62f9e60b
Vinay Rao <vrao8912@gmail.com>
2023-01-12 13:10:56 +0100

    comm

--- Frontend/hoppypolla/src/App.js
+++ Frontend/hoppypolla/src/App.js
@@ -1,29 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BrewRecipeContextProvider } from "./context/BrewRecipeContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
-//pages 
-import Home from './pages/Home'
-import Brew from './pages/Brew'
+//pages
+import Home from "./pages/Home";
+import Brew from "./pages/Brew";
 
 //components
 import Navbar from "./components/Navbar";
 import BrewRecipeDetails from "./components/BrewRecipeDetails";
-
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
-        <BrewRecipeContextProvider>
-          <div className="pages">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/brew" element={<Brew />} />
-              <Route path="/brew/:id" element={<BrewRecipeDetails />} />
-            </Routes>
-          </div>
-        </BrewRecipeContextProvider>
+        <Navbar />
+        <AuthContextProvider>
+          <BrewRecipeContextProvider>
+            <div className="pages">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/brew" element={<Brew />} />
+                <Route path="/brew/:id" element={<BrewRecipeDetails />} />
+              </Routes>
+            </div>
+          </BrewRecipeContextProvider>
+        </AuthContextProvider>
       </BrowserRouter>
     </div>
   );
@@ -31,11 +34,9 @@ function App() {
 
 export default App;
 
-
-
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// //pages 
+// //pages
 // import Home from './pages/Home'
 // import Brew from './pages/Brew'
 
@@ -43,7 +44,6 @@ export default App;
 // import Navbar from "./components/Navbar";
 // import BrewRecipeDetails from "./components/BrewRecipeDetails";
 // import BrewRecipesMap from "./components/BrewRecipesMap";
-
 
 // function App() {
 //   return (
